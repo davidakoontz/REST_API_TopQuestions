b7038c64edcdcbbc0f8bffb7c3182cd2660d4a53
David Koontz <david@koontz.name>
2024-12-28 14:29:45 -0800

    Example of REST API calls and JSON response using StackOverflow endPoint.

--- TopQuestions/TopQuestionsApp.swift
+++ TopQuestions/TopQuestionsApp.swift
@@ -3,7 +3,9 @@
 //  TopQuestions
 //
 //  Created by Matteo Manferdini on 12/09/23.
-//
+//  From Repo: https://github.com/matteom/TopQuestions
+//  See https://matteomanferdini.com/swift-rest-api/
+
 
 import SwiftUI
 
